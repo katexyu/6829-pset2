d3c0f1e9974433a21f7dffc5f541881d921b4410
Anish Athalye <me@anishathalye.com>
2016-10-26 22:40:27 -0400

    Add best parameters

---
+++ datagrump/parameters.hh
@@ -0,0 +1,11 @@
+#ifndef PARAMETERS_HH
+#define PARAMETERS_HH
+
+#define TARGET_95_PERCENTILE (70.0)
+#define K_P (5e-2)
+#define K_D (1e-3)
+#define CONTROL_EPOCH (20.0)
+#define HISTORY_SIZE (3)
+#define HISTORY_DECAY (0.8)
+
+#endif /* PARAMETERS_HH */
